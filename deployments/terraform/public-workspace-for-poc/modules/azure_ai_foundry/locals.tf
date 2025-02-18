952ff96b553f34913ec45d7b5709f2d0bc73781d
Shige Fukushima <shigeyf@outlook.com>
2025-02-18 15:09:20 +0900

    feat: 🎸 Use Azure naming module to truncate instance name

--- deployments/terraform/public-workspace-for-poc/modules/azure_ai_foundry/locals.tf
+++ deployments/terraform/public-workspace-for-poc/modules/azure_ai_foundry/locals.tf
@@ -1,10 +1,15 @@
 // modules/*/locals.tf
 
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = ">= 0.4.0"
+}
+
 locals {
   resource_group_name     = module.rg.resource.name
   resource_group_location = module.rg.resource.location
-  key_vault_name          = "${substr(replace(replace(var.key_vault_name, "-", ""), "kv", "kv-"), 0, 20)}${var.unique_suffix}"
-  storage_account_name    = "${substr(var.storage_account_name, 0, 20)}${var.unique_suffix}"
+  key_vault_name          = "${substr(replace(var.key_vault_name, "-", ""), 0, (module.naming.key_vault.max_length - var.unique_suffix_length))}${var.unique_suffix}"
+  storage_account_name    = "${substr(var.storage_account_name, 0, (module.naming.key_vault.max_length - var.unique_suffix_length))}${var.unique_suffix}"
 }
 
 locals {
