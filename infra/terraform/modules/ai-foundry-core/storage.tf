98deb8e48d8d2d1c44592a45d30cc249e17de1bc
Shige Fukushima <shigeyf@outlook.com>
2025-04-15 22:38:34 +0900

    fix: 🐛 Updated

--- infra/terraform/modules/ai-foundry-core/storage.tf
+++ infra/terraform/modules/ai-foundry-core/storage.tf
@@ -8,10 +8,11 @@ module "core_storage" {
   location            = var.location
   tags                = var.tags
 
-  storage_account_name    = var.storage_account_name
-  storage_uami_name       = var.storage_uami_name
-  keyvault_id             = module.core_kv.output.keyvault_id
-  customer_managed_key_id = module.core_storage_cmkey.output.key_versionless_id
+  storage_account_name          = var.storage_account_name
+  enable_user_assigned_identity = true
+  storage_uami_name             = var.storage_uami_name
+  keyvault_id                   = module.core_kv.output.keyvault_id
+  customer_managed_key_id       = module.core_storage_cmkey.output.key_versionless_id
 
   enable_public_network_access = var.enable_public_network_access
   private_endpoint_subnet_id   = var.private_endpoint_subnet_id
