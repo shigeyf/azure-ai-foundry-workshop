fa2a01d158afbd9b495273e05bc4de0bc75bf799
Shige Fukushima <shigeyf@outlook.com>
2025-04-17 03:02:49 +0900

    fix: 🐛 Fixed variable validation

--- infra/terraform/modules/ai-foundry-core/_variables.storage.tf
+++ infra/terraform/modules/ai-foundry-core/_variables.storage.tf
@@ -8,6 +8,12 @@ variable "storage_account_name" {
 variable "storage_uami_name" {
   description = "User-assigned Managed Identity name for the Storage"
   type        = string
+  default     = null
+
+  validation {
+    condition     = !(var.enable_user_assigned_identity && var.storage_uami_name == null)
+    error_message = "'storage_uami_name' must be set if 'enable_user_assigned_identity' is enabled."
+  }
 }
 
 variable "storage_private_endpoint_name" {
