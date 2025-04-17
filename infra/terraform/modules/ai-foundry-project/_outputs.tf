be4a8e62d74c28dbf77edab2db48e260aaf2b893
Shige Fukushima <shigeyf@outlook.com>
2025-04-18 00:41:09 +0900

    fix: 🐛 Fixed a bug related with identity

--- infra/terraform/modules/ai-foundry-project/_outputs.tf
+++ infra/terraform/modules/ai-foundry-project/_outputs.tf
@@ -2,9 +2,10 @@
 
 output "output" {
   value = {
-    ai_foundry_project_id           = azurerm_ai_foundry_project.this.id
-    ai_foundry_project_workspace_id = azurerm_ai_foundry_project.this.project_id
-    ai_foundry_project_uami_id      = var.enable_user_assigned_identity ? azurerm_user_assigned_identity.this[0].id : null
+    ai_foundry_project_id                     = azurerm_ai_foundry_project.this.id
+    ai_foundry_project_workspace_id           = azurerm_ai_foundry_project.this.project_id
+    ai_foundry_project_managed_identity       = azurerm_ai_foundry_project.this.identity[0].principal_id
+    ai_foundry_project_user_assigned_identity = var.enable_user_assigned_identity ? azurerm_user_assigned_identity.this[0].id : null
   }
   description = "Project resources for AI Foundry"
 }
--- infra/terraform/modules/ai-foundry-project/ai_project.tf
+++ infra/terraform/modules/ai-foundry-project/ai_project.tf
@@ -21,7 +21,7 @@ resource "azurerm_ai_foundry_project" "this" {
   # Enable system-assigned managed identity
   identity {
     type         = var.enable_user_assigned_identity ? "SystemAssigned, UserAssigned" : "SystemAssigned"
-    identity_ids = var.enable_user_assigned_identity ? [azurerm_user_assigned_identity.this[0].id] : null
+    identity_ids = var.enable_user_assigned_identity ? [azurerm_user_assigned_identity.this[0].id] : []
   }
 
   depends_on = [
